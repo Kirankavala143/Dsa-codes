4074df85fb78a81ebb843a12bba8285d7d5f2cd9
Kirankavala143 <kavalakiran98@gmail.com>
2025-07-13 22:37:54 +0530

    Adds functionality to find largest and second largest elements
    Implements logic to identify both the largest and second largest elements in an array.
    
    Replaces previous maximum element calculation with a more comprehensive approach to handle the second largest value.
    
    Enhances the utility of the script for array analysis.

--- Arrays.py
+++ Arrays.py
@@ -12,5 +12,22 @@
 # arr = [1, 2, 3, 4, 5]
 # print(largest_element(arr))
 
-l=[1,2,3,4,5]
-print(max(l))
+# a=[1, 2, 3, 4, 5]
+# largest = a[0]
+# for i in range(len(a)):
+#     if a[i] >largest:
+#         largest=a[i]
+# print(largest)
+
+# largest and second largest element in an array
+a=[1,2,3,4,5,7,7]
+largest=a[0]
+second_largest=a[0]
+for i in range(len(a)):
+    if a[i] > largest:
+        second_largest = largest
+        largest = a[i]
+    elif a[i] >second_largest and a[i]  !=largest:
+        second_largest = a[i]
+print("Largest element is:", largest)
+print("Second largest element is:", second_largest)
