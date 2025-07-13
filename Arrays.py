8072835057ebaf5ac871b446832381e62755b01a
Kirankavala143 <kavalakiran98@gmail.com>
2025-07-13 21:50:10 +0530

    Removes custom largest element function, uses max
    Replaces the custom implementation of finding the largest element in an array with the built-in `max` function for simplicity and efficiency.
    
    Commented out the original function and example to maintain reference while simplifying the codebase.

--- Arrays.py
+++ Arrays.py
@@ -1,13 +1,16 @@
 # largest element in an array
 # arr = [1, 2, 3, 4, 5]
 # print(max(arr))
-def largest_element(arr):
-    if not arr:
-        return None  # Handle empty array case
-    largest = arr[0]
-    for num in arr:
-        if num > largest:
-            largest = num
-    return largest
-arr = [1, 2, 3, 4, 5]
-print(largest_element(arr))
+# def largest_element(arr):
+#     if not arr:
+#         return None  # Handle empty array case
+#     largest = arr[0]
+#     for num in arr:
+#         if num > largest:
+#             largest = num
+#     return largest
+# arr = [1, 2, 3, 4, 5]
+# print(largest_element(arr))
+
+l=[1,2,3,4,5]
+print(max(l))
