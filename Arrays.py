8b81af900e407334338147a52f83a4d1466a39c8
Kirankavala143 <kavalakiran98@gmail.com>
2025-07-14 13:32:17 +0530

    first commit

--- Arrays.py
+++ Arrays.py
@@ -111,3 +111,14 @@ zero_count = n.count(0)
 lis.extend([0] * zero_count)
 
 print(lis)
+
+# optimal solution
+nums=[0,1,0,3,12]
+pos = 0  # position to place the next non-zero
+for i in range(len(nums)):
+    if nums[i] != 0:
+        nums[pos] = nums[i]
+        pos += 1    
+for i in range(pos, len(nums)):
+    nums[i] = 0
+print(nums)
