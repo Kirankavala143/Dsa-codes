94baf5c2f8239331b19816f2517b941e0756ae87
Kirankavala143 <kavalakiran98@gmail.com>
2025-07-15 11:46:21 +0530

    first commit

--- recursion.py
+++ recursion.py
@@ -16,3 +16,31 @@
 #     printNto1(n-1)
 # printNto1(5)
 
+# sum of first n numbers
+# def sumN(n):
+#     if n==1:
+#         return 1
+#     return n + sumN(n-1)
+# print(sumN(5))
+
+# factorial
+# def fact(n):
+#     if n==1:
+#         return 1
+#     return n * fact(n-1)
+# print(fact(5))    
+
+# reverse a array
+
+
+# def reverse(arr):
+#     if not arr:  # Base case: empty list
+#         return []
+#     return [arr[-1]] + reverse(arr[:-1])
+
+# print(reverse([1, 2, 3, 4, 5]))  # Output: [5, 4, 3, 2, 1]
+
+# def reverse(arr):
+#     return arr[::-1]
+
+# print(reverse([1, 2, 3, 4, 5]))
