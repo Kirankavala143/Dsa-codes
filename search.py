06469188c947e096f14f7295bd719b531b61dc94
Kirankavala143 <kavalakiran98@gmail.com>
2025-07-16 10:01:35 +0530

    first commit

--- search.py
+++ search.py
@@ -20,16 +20,24 @@
 #     arr[i],arr[min_index]=arr[min_index],arr[i]
 # print(arr)
 
-# another way
+# bubble sort  (compare beside numbers)
+# arr=[3,24,7,87,5,9,78]
+# n=len(arr)
+# for i in range(n-1):
+#     for j in range(n-i-1):
+#         if arr[j+1]<arr[j]:
+#             arr[j],arr[j+1]=arr[j+1],arr[j]
+# print(arr)
 
-
-arr=[4,32,3,34,46,54,12,45]
+# insertion sort(insert elements at right position)
+arr=[3,24,7,87,5,9,78]
 n=len(arr)
-for i in range(n):
-    min_index=i
-    for j in range(i+1,n):
-        if arr[j]<arr[min_index]:
-            min_index=j
-        arr[i],arr[min_index]=arr[min_index],arr[i]
+for i in range(1,n):
+    key=arr[i]
+    j=i-1
+    while j>=0 and key<arr[j]:
+        arr[j+1]=arr[j]
+        j-=1
+    arr[j+1]=key
 print(arr)
 
