48beac910408038852ad40d97992be24a33fcbb3
Kirankavala143 <kavalakiran98@gmail.com>
2025-07-17 22:03:28 +0530

    first commit

--- extra.py
+++ extra.py
@@ -190,3 +190,21 @@ for i in nums:
 #             if dic[i]> len(nums)//3:
 #                 result.append(i)
 #         return result
+
+# 205
+# class Solution:
+#     def isIsomorphic(self, s: str, t: str) -> bool:
+#         char_index_s = {}
+#         char_index_t = {}
+
+#         for i in range(len(s)):
+#             if s[i] not in char_index_s:
+#                 char_index_s[s[i]] = i
+
+#             if t[i] not in char_index_t:
+#                 char_index_t[t[i]] = i
+            
+#             if char_index_s[s[i]] != char_index_t[t[i]]:
+#                 return False
+
+#         return True
