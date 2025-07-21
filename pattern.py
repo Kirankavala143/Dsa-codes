fccb7a492a051ad1284a9c1edcc4cf32dc023f0d
Kirankavala143 <kavalakiran98@gmail.com>
2025-07-21 14:30:52 +0530

    first commit

--- pattern.py
+++ pattern.py
@@ -7,12 +7,35 @@
 #             print("-",end="")
 #     print()
 
+# r=5
+# c=5
+# for i in range(r):
+#     for j in range(c):
+#         print("*",end="")
+#         if j!=c-1:
+#             print("-",end="")
+#     print()
+
+# square matrix 
 r=5
 c=5
 for i in range(r):
-    if i ==0 or i==-1:
-        print("*")
     for j in range(c):
-        print("*",end="")
+        if i==0 or i==r-1 or j==0 or j==c-1:
+            print("*",end="")
+        else:
+            print(" ",end="")
+        # if j!=c-1:
+        #     print(" ",end="")
     print()
-    
+
+# r=5
+# c=20
+# for i in range(r):
+#     for j in range(c):
+#         if i==0 or i==r-1 or j==0 or j==c-1:
+#             print("*",end="")
+#         else:
+#             print(" ",end="")
+#     print()
+
