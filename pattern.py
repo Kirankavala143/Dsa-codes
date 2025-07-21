b8e7989145ac54b8e0a7463146b007cf91bd54af
Kirankavala143 <kavalakiran98@gmail.com>
2025-07-21 13:36:45 +0530

    first commit

---
+++ pattern.py
@@ -0,0 +1,18 @@
+# r=3
+# c=7
+# for i in range(r):
+#     for j in range(c):
+#         print("*",end="")
+#         if j!=c-1:
+#             print("-",end="")
+#     print()
+
+r=5
+c=5
+for i in range(r):
+    if i ==0 or i==-1:
+        print("*")
+    for j in range(c):
+        print("*",end="")
+    print()
+    
