647c23ec113fd9a99e137a31c6a3a46206534610
Kirankavala143 <kavalakiran98@gmail.com>
2025-09-26 14:33:12 +0530

    first commit

--- DS/stt.py
+++ DS/stt.py
@@ -1,29 +1,38 @@
-# def two_sum_sorted(arr, target):
-#     left, right = 0, len(arr) - 1
-#     while left < right:
-#         curr_sum = arr[left] + arr[right]
-#         if curr_sum == target:
-#             return (left, right)
-#         elif curr_sum < target:
-#             left += 1  # need bigger sum
-#         else:
-#             right -= 1  # need smaller sum
-#     return None
-# arr=[1,3,6,7,9,12,15,25]
-# target=18
-# print(two_sum_sorted(arr, target))
+def two_sum_sorted(arr, target):
+    left, right = 0, len(arr) - 1
+    while left < right:
+        curr_sum = arr[left] + arr[right]
+        if curr_sum == target:
+            return (left, right)
+        elif curr_sum < target:
+            left += 1  # need bigger sum
+        else:
+            right -= 1  # need smaller sum
+    return None
+arr=[1,3,6,7,9,12,15,25]
+target=18
+print(two_sum_sorted(arr, target))
 
 
-def has_cycle(head):
-    slow, fast = head, head
-    while fast and fast.next:
-        slow = slow.next
-        fast = fast.next.next
-        if slow == fast:
-            return True
-    return False
+# def has_cycle(head):
+#     slow, fast = head, head
+#     while fast and fast.next:
+#         slow = slow.next
+#         fast = fast.next.next
+#         if slow == fast:
+#             return True
+#     return False
 
-print(has_cycle())
+# print(has_cycle())
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i,j]
+        
 
 
 
