40c6a4b32627116f0c8a6c8854d8b809ac22c73d
Kirankavala143 <kavalakiran98@gmail.com>
2025-12-09 18:38:05 +0530

    xsf

--- pattern.py
+++ pattern.py
@@ -193,3 +193,11 @@ for i in range(r-1,-1,-1):
 #         else:
 #             print(" ",end="")
 #     print()
+
+
+#     for j in range(temp):
+#         if j==0 or j==temp-1 or i==r-1:
+#             print("*",end="")
+#         else:
+#             print(" ",end="")
+#     print()
